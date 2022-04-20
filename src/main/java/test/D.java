92aef273a6745aec271c7d177cdcff2226fd530a
beton726 <909043377fas>
2022-04-21 02:14:32 +0300

    2 в тратата

--- src/main/java/test/D.java
+++ src/main/java/test/D.java
@@ -2,19 +2,7 @@ package test;
 
 public class D {
 
-    public void test_1() {
-        System.out.println();
-    }
-
-    public void test_2() {
-        System.out.println();
-    }
-
-    public void test_3() {
-        System.out.println();
-    }
-
-    public void test_4() {
+    public void test() {
         System.out.println();
         System.out.println(123);
         System.out.println(333);
