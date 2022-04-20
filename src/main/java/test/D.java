c0a8715629cfd9c5e01bcacbc0cc12ea04b6434e
beton726 <909043377fas>
2022-04-21 02:16:11 +0300

    2 в тратата

--- src/main/java/test/D.java
+++ src/main/java/test/D.java
@@ -17,4 +17,12 @@ public class D {
     public void test_4() {
         System.out.println();
     }
+
+    public void test_5() {
+        System.out.println();
+    }
+
+    public void test_6() {
+        System.out.println();
+    }
 }
