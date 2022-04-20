98873ab1c7980b5dfcaaca05cbb865e3ab5b98f4
beton726 <909043377fas>
2022-04-21 02:00:45 +0300

    1 в

--- src/main/java/test/D.java
+++ src/main/java/test/D.java
@@ -2,7 +2,11 @@ package test;
 
 public class D {
 
-    public void test() {
+    public void test_1() {
+        System.out.println();
+    }
+
+    public void test_2() {
         System.out.println();
     }
 }
